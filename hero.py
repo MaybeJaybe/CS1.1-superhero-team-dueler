3606ea2c93f6cae44f125820dc919d86679e7134
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-02-17 22:05:27 -0800

    check if alive

--- hero.py
+++ hero.py
@@ -1,22 +1,41 @@
 import random
+from ability import Ability
+from armor import Armor
 
 class Hero:
-    # needs:
-    # init parameters: name:string, starting_health:int=100
-    # add_ability parameters: ability:Ability Object
-    # attack: no parameters
-    # defend: incoming_damage:int
-    # take_damage parameters: damage
-    # is alive: no parameters
-    # fight parameters: opponent: Hero Class
-
-
     def __init__(self, name, starting_health=100):
         # need default starting health, can set it in header
         self.name = name
         self.starting_health = starting_health
         # current health is same as starting health cuz no damage yet
         self.current_health = starting_health
+        # abilities list and armors list
+        self.abilities = list()
+        self.armors = list()
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def add_ability(self, ability):
+        # add ability objects
+        self.abilities.append(ability)
+
+    def defend(self):
+        total_blocked = 0
+        for armor in self.armors:
+            total_blocked += armor.block()
+        return total_blocked
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def take_damage(self, damage):
+        final_damage = damage - self.defend()
+        if final_damage > 0:
+            self.current_health -= final_damage
 
     def fight(self, opponent):
         # take turns fighting until a victor emerges
@@ -24,11 +43,44 @@ class Hero:
         hero_list = [self.name, opponent.name]
         print(random.choice(hero_list) + " won the fight!")
 
-if __name__ == "__main__":
-    # my_hero = Hero("Grace Hopper", 200)
-    # print(my_hero.name)
-    # print(my_hero.current_health)
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
+    def is_alive(self):
+        # check current health of hero, if <=0, return false, otherwise return true
+        if self.current_health > 0:
+            return True
+        else: 
+            return False
 
-    hero1.fight(hero2)
+
+if __name__ == "__main__":
+    hero = Hero("Grace Hopper", 200)
+
+    ability1 = Ability("Great Debugging", 50)
+    hero.add_ability(ability1)
+    ability2 = Ability("Hopper Smash", 20)
+    hero.add_ability(ability2)
+
+    armor1 = Armor("Cloak of Shadows", 50)
+    hero.add_armor(armor1)
+
+
+    print(hero.current_health)
+    hero.take_damage(50)
+    print(hero.current_health)
+    print(hero.is_alive())
+    print("180 damage")
+    hero.take_damage(180)
+    print(hero.current_health)
+    print(hero.is_alive())
+    
+    # print(hero.attack())
+    # print(hero.defend())
+
+    # print(hero.name)
+    # print(hero.current_health)
+    # print(hero.abilities)
+
+
+    # hero1 = Hero("Wonder Woman")
+    # hero2 = Hero("Dumbledore")
+
+    # hero1.fight(hero2)
